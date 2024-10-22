212a8e01df12ad344596dbd1eb7caee943359c13
Mega <ravisit121@gmail.com>
2024-10-22 08:42:57 +0700

    create aplikasi segitiga

---
+++ segitiga.py
@@ -0,0 +1,16 @@
+# Dibuat oleh : Moch keanu alvino
+# Tanggal pengerjaan : 22 oktober 2024
+# Program menghitung rumus segitiga
+
+print("="*40)
+print("PROGRAM SEGITIGA")
+print("="*40)
+
+tinggi = float(input("Masukkan Tinggi : "))
+alas = float(input("Masukkan Alas : "))
+
+luas = alas * tinggi /2
+keliling = alas * 3
+
+print("luas",luas, "cm2")
+print("keliling",keliling, "cm")
